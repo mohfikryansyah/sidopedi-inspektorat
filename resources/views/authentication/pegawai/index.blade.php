c47f2466ef3a96f25369ab3053e2fa1e842216d9
mohfikryansyah <moh.fikryansyah@gmail.com>
2023-12-12 13:46:09 +0800

    roles

--- resources/views/authentication/pegawai/index.blade.php
+++ resources/views/authentication/pegawai/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     @include('layouts.partials.alert-success')
     <div class="md:grid md:space-y-0 space-y-4 grid-cols-7 gap-5">
-        @role('PEGAWAI')
+        @role('ADMIN')
         @include('authentication.pegawai.LaporanResource.create')
         @endrole
         @include('authentication.pegawai.LaporanResource.table-laporan')
