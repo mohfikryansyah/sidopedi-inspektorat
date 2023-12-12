11edcafc9bec9d638343f4b1f38d83d0f8735afd
mohfikryansyah <moh.fikryansyah@gmail.com>
2023-12-12 09:26:28 +0800

    tes 2

--- resources/views/authentication/pegawai/index.blade.php
+++ resources/views/authentication/pegawai/index.blade.php
@@ -2,10 +2,9 @@
 @section('content')
     @include('layouts.partials.alert-success')
     <div class="md:grid md:space-y-0 space-y-4 grid-cols-7 gap-5">
-        @if (auth()->user()->hasRole('ADMIN'))
+        @if (auth()->user()->hasRole('PEGAWAI'))
             @include('authentication.pegawai.LaporanResource.create')
         @endif
-    @endrole
     @include('authentication.pegawai.LaporanResource.table-laporan')
 </div>
 @endsection
