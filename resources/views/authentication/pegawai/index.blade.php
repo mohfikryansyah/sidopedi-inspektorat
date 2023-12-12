8d25d8ed0e519ad9397aa91ac14e2ebdb8e92bdd
mohfikryansyah <moh.fikryansyah@gmail.com>
2023-12-12 13:47:30 +0800

    coba

--- resources/views/authentication/pegawai/index.blade.php
+++ resources/views/authentication/pegawai/index.blade.php
@@ -4,6 +4,8 @@
     <div class="md:grid md:space-y-0 space-y-4 grid-cols-7 gap-5">
         @role('ADMIN')
         @include('authentication.pegawai.LaporanResource.create')
+        @else
+        <p>pegawai</p>
         @endrole
         @include('authentication.pegawai.LaporanResource.table-laporan')
     </div>
