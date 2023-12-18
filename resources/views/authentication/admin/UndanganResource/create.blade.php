29946ce173b12f3d88dc0d0417f373611e2101da
mohfikryansyah <moh.fikryansyah@gmail.com>
2023-12-18 14:16:25 +0800

    margin

--- resources/views/authentication/admin/UndanganResource/create.blade.php
+++ resources/views/authentication/admin/UndanganResource/create.blade.php
@@ -4,7 +4,7 @@
         <form method="POST" action="{{ route('undangan.store') }}" enctype="multipart/form-data" class="mb-0">
             @csrf
 
-            <div class="mt-4 mb-5">
+            <div class="mb-5">
                 <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="undangan">Upload Undangan</label>
                 <input
                     class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
@@ -15,7 +15,7 @@
             </div>
 
             <button type="submit"
-                class="text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Kirim</button>
+                class="text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
         </form>
     </div>
 </div>
