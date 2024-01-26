2afdbb44c92bdcb39ec6abd7f88a608c32962365
mohfikryansyah <moh.fikryansyah@gmail.com>
2024-01-26 16:08:14 +0800

    fix st admin

--- resources/views/authentication/admin/SuratTugasResource/edit.blade.php
+++ resources/views/authentication/admin/SuratTugasResource/edit.blade.php
@@ -28,7 +28,7 @@
                     <div class="mb-5">
                         <label for="judul" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Judul
                             Undangan</label>
-                        <input type="text" id="judul" name="judul" value="{{ old('judul', $undangan->judul) }}"
+                        <input type="text" id="judul" name="judul" value="{{ old('judul', $surat_tuga->judul) }}"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         @error('judul')
                             <p>{{ $message }}</p>
