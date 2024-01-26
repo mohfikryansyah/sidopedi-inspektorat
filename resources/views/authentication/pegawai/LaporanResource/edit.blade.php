3c2fe0f26a8bff06c109e5b88266734083f1d5d0
mohfikryansyah <moh.fikryansyah@gmail.com>
2024-01-26 16:04:25 +0800

    fix laporan tida bisa m edit

--- resources/views/authentication/pegawai/LaporanResource/edit.blade.php
+++ resources/views/authentication/pegawai/LaporanResource/edit.blade.php
@@ -29,13 +29,13 @@
                     </div>
 
                     <div class="mb-5">
-                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="surat_tugas">Upload
+                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="laporan">Upload
                             Laporan</label>
                         <input
                             class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                            id="surat_tugas" name="surat_tugas" type="file" aria-describedby="file_input_help">
+                            id="laporan" name="laporan" type="file" aria-describedby="file_input_help">
                         <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">PDF (MAX. 1MB).</p>
-                        @error('surat_tugas')
+                        @error('laporan')
                             <p>{{ $message }}</p>
                         @enderror
                     </div>
