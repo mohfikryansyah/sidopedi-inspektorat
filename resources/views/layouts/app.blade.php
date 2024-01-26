008cb181a1df5eb608d628f99e90801bd28197c1
mohfikryansyah <moh.fikryansyah@gmail.com>
2024-01-26 14:55:54 +0800

    name app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'SIDOPEDI - Inspektorat') }}</title>
+    <title>SIDOPEDI - Inspektorat</title>
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
 
